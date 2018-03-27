42f411eb51bad9a291d9fe01646b14bc30423ff6
adsantos97 <asantos@cs.loyola.edu>
2018-03-27 10:03:26 -0400

    finished tree

--- tree.hs
+++ tree.hs
@@ -11,14 +11,14 @@ bst = Node(10,
 iit Nil = []
 iit (Node(v,l,r)) = (iit l) ++ [v] ++ (iit r)
 
-found needle Nil = False
-found needle (Node(v, l, r))
- | needle == v = True
- | needle < v = found needle l
- | otherwise = found needle r
+--found needle Nil = False
+found needle (Node(v, l, r)) = if needle == v then True
+                               else False
 
 done needle Nil = True
 done needle (Node(v, l, r)) = False
+
+thead (Node(v, l, r)) = v
 
 search needle Nil = []
 search needle (Node(v, l, r))
@@ -26,8 +26,11 @@ search needle (Node(v, l, r))
  | needle < v = search needle l
  | otherwise = search needle r
 
+tnext needle (Node(v, l, r))
+ | needle < v = l
+ | otherwise = r
 
-
-search2 needle haystack done found
+search2 haystack needle current_item done found next
  | done needle haystack = []
- | found
+ | found needle haystack = [(current_item haystack)]
+ | otherwise = search2 (next needle haystack) needle current_item done found next
