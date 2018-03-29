31fe07aa9840b939d56ace8141ba9671bd8d34d2
adsantos97 <asantos@cs.loyola.edu>
2018-03-29 16:29:25 -0400

    fib

--- fib.hs
+++ fib.hs
@@ -2,21 +2,22 @@ fibonacci 0 = 0
 fibonacci 1 = 1
 fibonacci n = fibonacci (n - 1) + fibonacci (n - 2)
 
---fib n = [fibonacci n | n <- [0..(n-1)]]
---fib n
--- | n == 0 = []
--- | n < 10 = (fibonacci n) : fib (n-1)
-
-{-fib2 n
- | n == 0 = []
- | n < 10 = (fibonacci n) : fib2 (n-1)
--}
-
 fib n
- | n == 0 = [fibonacci n]
+ | n == 0 = [0]
  | otherwise = fib (n-1) ++ [fibonacci n]
 
-range a b
- | a == b = []
- | a > b = a : range (a-1) b
- | a < b = a : range (a+1) b
+data Seq a = Nil | Cons (a, (() -> Seq a))
+
+instance (Show a) => Show (Seq a) where
+  show Nil = "Nil"
+  show (Cons (a, f)) = "Cons(" ++ show a ++ ", fn())"
+
+hd Nil = error "HD_NIL"
+hd (Cons (x, f)) = x
+
+tl Nil = error "TL_NIL"
+tl (Cons (x, f)) = f()
+
+from k = Cons(k, \() -> from (k+1))
+
+
