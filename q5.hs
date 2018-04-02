5206f351978c391e971adf924b0735b44a5dd3d4
adsantos97 <asantos@cs.loyola.edu>
2018-04-02 16:52:15 -0400

    smallest funct

--- q5.hs
+++ q5.hs
@@ -1,12 +1,10 @@
 unsorted_list = [6, 2, 7, 1]
 
 -- non-tail recursive of smallest function
-smallest [x] = x
-smallest lst = if (head lst) < (head (tail lst))
-                 then smallest lst
-               else smallest ((head (tail lst)):(tail lst))
+--smallest lst = minimum lst
 
 -- tail recursive of smallest function
-small [x] = x
-small (x:y:xs) = if x < y then small (x:xs)
-                 else small (y:xs)
+smallest [x] = x
+smallest (x:xs)
+ | x < (smallest xs) = x
+ | otherwise = smallest xs
